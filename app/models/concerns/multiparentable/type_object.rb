e7da2fbe7c83040da9ada957b8ea55bea70f7d26
CaptainPhilipp <CaptainPhilipp@users.noreply.github.com>
2017-06-26 12:32:39 +0300

    some TypeObject features

--- app/models/concerns/multiparentable/type_object.rb
+++ app/models/concerns/multiparentable/type_object.rb
@@ -1,35 +1,47 @@
 module Multiparentable
   class TypeObject
-    def initialize(type:, klass:)
-      @string_type = type  if type.is_a?  String
-      @klass       = klass if klass.is_a? Class
+    def initialize(type: nil, klass: nil, table: nil)
+      @string_type    = type  if type.is_a?  String
+      @string_table   = table if type.is_a?  String
+      @symbol_table   = table if type.is_a?  Symbol
+      @constant_class = klass if klass.is_a? Class
       check_arguments
     end
 
     # Constant
     def klass
-      @klass ||= type.constantize
+      @klass ||= @constant_class || type.constantize
     end
-
-    # Classified
-    def type
-      @type ||= string.classify || klass.to_s
-    end
-
-    # tableized
+    
+    # Symbol or tableized
     def table
-      @table ||= (string && string.tableize) || type.tableize
+      @any_table ||= @symbol_table || table_str
+    end
+    
+    # classified
+    def type
+      @type ||= @string_type || @string_table.classify || @constant_class.to_s
+    end
+    
+    # Symbol
+    def table_sym
+      @table_sym ||= @symbol_table || table_str.to_sym
+    end
+    
+    # tableized
+    def table_str
+      @table ||= @string_table || any_string.tableize
     end
 
     private
-
-    def string
-      @string_type
+    
+    def any_string
+      @string ||= string_table || string_type || constant_class.to_s || @symbol_table.to_s
     end
 
     def check_arguments
       return self if @klass || @string_type
-      raise ArgumentError, 'childrens(type: `String`) or childrens(klass: `Class`) must be setted'
+      raise ArgumentError, "(type: 'Classified'), (klass: Constant) or (table: 'tableized') must be setted"
     end
   end
 end
