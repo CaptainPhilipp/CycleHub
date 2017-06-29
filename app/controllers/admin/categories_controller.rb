23a0acda7c5d3997bb6e7f432d24f79024206c21
CaptainPhilipp <Ph-s@mail.ru>
2017-06-29 17:59:21 +0300

    categories controller

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,24 @@
+class Admin
+  class CategoriesController < BaseController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'categories/index'
+  end
+
+  namespace :admin do
+    get 'categories/show'
+  end
+
+  namespace :admin do
+    get 'categories/new'
+  end
+
+  namespace :admin do
+    get 'categories/create'
+  end
+
+  namespace :admin do
+    get 'categories/edit'
+  end
+
+  namespace :admin do
+    get 'categories/update'
+  end
+
+  namespace :admin do
+    get 'categories/destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
