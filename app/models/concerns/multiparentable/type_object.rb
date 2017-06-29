5b8292aaf6f9ed915e20c7e9cdfa4a44d5ed6d94
CaptainPhilipp <Ph-s@mail.ru>
2017-06-29 07:55:48 +0300

    remove unnecessary responsipility

--- app/models/concerns/multiparentable/type_object.rb
+++ app/models/concerns/multiparentable/type_object.rb
@@ -1,11 +1,14 @@
 module Multiparentable
   class TypeObject
-    def initialize(type: nil, klass: nil, table: nil)
+    def initialize(type: nil, klass: nil)
       @string_type    = type.is_a?(String) ? type  : nil
-      @string_table   = type.is_a?(String) ? table : nil
-      @symbol_table   = type.is_a?(Symbol) ? table : nil
       @constant_class = klass.is_a?(Class) ? klass : nil
-      check_arguments
+      check_arguments!
+    end
+
+    # Symbol or tableized
+    def table
+      @table ||= klass.table_name
     end
 
     # Constant
@@ -13,24 +16,15 @@ module Multiparentable
       @klass ||= @constant_class || type.constantize
     end
 
-    # Symbol or tableized
-    def table
-      @table ||= @symbol_table || @string_table || klass.table_name
-    end
-
     # classified
     def type
-      @type ||= @string_type || @constant_class.try(:to_s) || table_to_s.try(:classify)
+      @type ||= @string_type || @constant_class.try(:to_s)
     end
 
     private
 
-    def table_to_s
-      @string_table || (@symbol_table && @symbol_table.to_s)
-    end
-
-    def check_arguments
-      return self if @constant_class || @string_type || @string_table || @symbol_table
+    def check_arguments!
+      return self if @constant_class || @string_type
       raise ArgumentError, "(type: 'Classified'), (klass: Constant) or (table: 'tableized') must be setted"
     end
   end
