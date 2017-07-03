16cbdc12ca9afda0ba0610d090b4f915b1ed190c
CaptainPhilipp <Ph-s@mail.ru>
2017-07-03 19:33:37 +0300

    Depth Value Object

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,3 @@
 class Category < ApplicationRecord
   include Multiparentable
-
-  def depth_name
-    I18n.t (depth || 'nil'), scope: 'category.attributes.depth_index', locale: :en
-  end
 end
---
+++ app/services/value/depth.rb
@@ -0,0 +1,57 @@
+module Value
+  class Depth
+    NIL = 'nil'.freeze
+
+    def initialize(input = nil)
+      @depth =
+        case input
+        when nil             then NIL
+        when String, Integer then input
+        else input.depth
+        end
+    end
+
+    def value
+      @depth
+    end
+
+    def ru_title
+      title(:ru)
+    end
+
+    def en_title
+      title(:en)
+    end
+
+    def title(locale)
+      self.class.title(depth, locale)
+    end
+
+    def options_for_select(locale)
+      self.class.options_for_select(locale)
+    end
+
+    class << self
+      def options_for_select(locale)
+        [ [title(NIL, locale), nil],
+          [title(0,   locale), 0],
+          [title(1,   locale), 1],
+          [title(2,   locale), 2] ]
+      end
+
+      def title(depth, locale)
+        I18n.t(depth_key(depth), scope: 'category.attributes.depth_index', locale: locale)
+      end
+
+      private
+
+      def depth_key(depth)
+        depth == NIL ? NIL : "depth_#{depth}"
+      end
+    end
+
+    private
+
+    attr_reader :depth
+  end
+end
