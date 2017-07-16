2f3d62a2852fefb08d4c87b05822e80b56060003
CaptainPhilipp <Ph-s@mail.ru>
2017-07-16 23:31:12 +0300

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,11 @@
 require 'application_responder'
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :set_locale
+
   self.responder = ApplicationResponder
   respond_to :html
-
-  protect_from_forgery with: :exception
-
-  before_action :set_locale
 
   # method overload for url helper
   def default_url_options
