e3329a4a6735ffbc1461077c3cea927bfc56495d
CaptainPhilipp <Ph-s@mail.ru>
2017-07-16 23:10:20 +0300

    locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  # method overload for url helper
+  def default_url_options
+    { locale: set_locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || extract_from_header || I18n.default_locale
+  end
+
+  def extract_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'categories/show'
-
-  namespace :admin do
-    resources :categories, only: %i[index create update destroy] do
-      get :tree, on: :collection
+  scope '(:locale)', locale: /en|ru/ do
+    namespace :admin do
+      resources :categories, only: %i[index create update destroy] do
+        get :tree, on: :collection
+      end
     end
-  end
 
-  resources :categories, only: %i[index show]
+    resources :categories, only: %i[index show]
+
+    get '/', to: 'categories#index', as: :home
+  end
 
   root to: 'categories#index'
 end
